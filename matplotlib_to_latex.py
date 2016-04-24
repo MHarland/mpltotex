15c96d2bdfd7ac8790b82e59952a9158c1e4b1ee
Malte Harland <malte@PHYSnet-RZs-Air.fritz.box>
2016-04-24 16:39:53 +0200

    add prl parameters, all remaining to be fixed

--- matplotlib_to_latex.py
+++ matplotlib_to_latex.py
@@ -2,6 +2,31 @@
 from matplotlib import pyplot as plt
 from numpy import sqrt
 
+def set_prl_parameters(monitor_dpi = 150, fig_width_pt = 510., height_width_ratio = False, font_size = 10, font_size_small = 10):
+    fig_width = fig_width_pt/monitor_dpi
+    if height_width_ratio:
+        fig_height = fig_width * height_width_ratio
+    else:
+        golden_mean = (sqrt(5)-1.0)/2.0
+        fig_height = fig_width * golden_mean
+    fig_size = [fig_width, fig_height]
+    #font = "Times New Roman"
+    params = {'backend': 'ps',
+              'font.size': font_size,
+              'legend.fontsize': font_size,
+              'axes.labelsize': font_size,
+              'xtick.labelsize': font_size_small,
+              'ytick.labelsize': font_size_small,
+              'text.usetex': True,
+              'figure.figsize': fig_size
+              #'font.family': font,
+              #'font.serif': 'Roman'
+              #'font.sans-serif': 'Helvetica'
+              #'mathtext.fontset': 'custom',
+              #'mathtext.rm': font
+              }
+    plt.rcParams.update(params)
+    
 def set_masterthesis_parameters(height_factor = 1):
     to_high_res = 300/72.27
     font_size = 12
@@ -16,13 +41,15 @@ def set_masterthesis_parameters(height_factor = 1):
     params = {'backend': 'pdf',
               'axes.labelsize': font_size,
               'font.size': font_size,
+              'text.fontsize': font_size,
               'legend.fontsize': font_size,
               'xtick.labelsize': font_size_small,
               'ytick.labelsize': font_size_small,
+              'axes.labelsize': font_size,
               'text.usetex': True,
-              'figure.figsize': fig_size,
-              'font.family': 'serif',
-              'font.serif': 'Times New Roman'
+              'figure.figsize': fig_size
+              #'font.family': 'serif',
+              #'font.serif': 'Times New Roman'
               }
     plt.rcParams.update(params)
 
