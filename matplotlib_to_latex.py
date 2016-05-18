6b3156394784a0c895334119a64d4408371108a9
Malte Harland <malte@dhcp-172-21-62-185.wlan.uni-hamburg.de.local>
2016-05-18 09:07:24 +0200

    update set_log_parameters

--- matplotlib_to_latex.py
+++ matplotlib_to_latex.py
@@ -53,26 +53,24 @@ def set_masterthesis_parameters(height_factor = 1):
               }
     plt.rcParams.update(params)
 
-def set_phdlog_parameters(height_factor = 1):
-    to_high_res = 300/72.27
-    font_size = 12
-    font_size_small = 8
-    fig_width_pt = 390. * to_high_res
-    inches_per_pt = 1.0/300.
-    golden_mean = (sqrt(5)-1.0)/2.0
-    fig_width = fig_width_pt * inches_per_pt
-    fig_height = fig_width * golden_mean * height_factor
+def set_log_parameters(monitor_dpi = 150, fig_width_pt = 390., height_width_ratio = False, font_size = 12, font_size_small = 8):
+    fig_width = fig_width_pt/monitor_dpi
+    if height_width_ratio:
+        fig_height = fig_width * height_width_ratio
+    else:
+        golden_mean = (sqrt(5)-1.0)/2.0
+        fig_height = fig_width * golden_mean
     fig_size = [fig_width, fig_height]
-    params = {'backend': 'pdf',
+    params = {'backend': 'ps',
               'axes.labelsize': font_size,
               'font.size': font_size,
               'legend.fontsize': font_size_small,
               'xtick.labelsize': font_size_small,
               'ytick.labelsize': font_size_small,
               'text.usetex': True,
-              'figure.figsize': fig_size,
-              'font.family': 'serif',
-              'font.serif': 'Computer Modern Roman'
+              'figure.figsize': fig_size
+              #'font.family': 'serif',
+              #'font.serif': 'Computer Modern Roman'
               }
     plt.rcParams.update(params)
 
